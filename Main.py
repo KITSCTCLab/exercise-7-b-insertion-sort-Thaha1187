eede3c40f23a85e9b262a93d6b9a1515ac2cea82
Thaha1187 <110010500+Thaha1187@users.noreply.github.com>
2022-09-13 11:23:09 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,14 +2,14 @@ from typing import List
 
 def insertionSort(A) -> List[int]:
   # Write your code here
-  for i in range (1,len(A)):
+  for i in range(1,len(A)):
     v = A[i]
     j = i-1
     while j>=0 and A[j] > v :
       A[j+1] = A[j]
       j = j-1
     A[j+1] = v
-  return A[j+1]
+  return A
 
 # data = [9, 5, 1, 4, 3]
 input_data = input()
